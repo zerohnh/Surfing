554cac790798142f7f06c7cc42de457334fa833c
MoGuangYu <staryuboy@gmail.com>
2025-05-04 20:18:41 +0800

    update uninstall.sh
    Delete corresponding components and services when uninstalling modules

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,14 @@ rm -f /data/adb/service.d/Surfing_service.sh 2>/dev/null
 rm -f /data/adb/ksu/service.d/Surfing_service.sh 2>/dev/null
 rm -rf /data/adb/box_bll 2>/dev/null
 
-rm -rf /data/adb/modules/surfingtile 2>/dev/null
-rm -rf /data/adb/modules_update/Surfingtile 2>/dev/null
-rm -f /data/adb/modules_update/Surfingtile/update 2>/dev/null
+rm -rf /data/adb/modules/Surfingtile 2>/dev/null
+
+APP_DIR=$(find /data/app -type d -name "*com.yadli.surfingtile*" 2>/dev/null | grep com.yadli.surfingtile)
+rm -rf "$APP_DIR"
+rm -rf /data/user/0/com.yadli.surfingtile 2>/dev/null
+rm -rf /data/data/com.yadli.surfingtile 2>/dev/null
+
+APP_DIR2=$(find /data/app -type d -name "*com.android64bit.web*" 2>/dev/null | grep com.android64bit.web)
+rm -rf "$APP_DIR2"
+rm -rf /data/user/0/com.android64bit.web 2>/dev/null
+rm -rf /data/data/com.android64bit.web 2>/dev/null
