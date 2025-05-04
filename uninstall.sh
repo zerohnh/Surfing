b38a19a0c9fb7333eebbcb5f87ce70bb9bb9bb0d
MoGuangYu <staryuboy@gmail.com>
2025-05-04 15:13:08 +0800

    update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,9 @@
 #!/system/bin/sh
 
-rm -f "/data/adb/service.d/Surfing_service.sh" 2>/dev/null
+rm -f /data/adb/service.d/Surfing_service.sh 2>/dev/null
+rm -f /data/adb/ksu/service.d/Surfing_service.sh 2>/dev/null
 rm -rf /data/adb/box_bll 2>/dev/null
+
+rm -rf /data/adb/modules/surfingtile 2>/dev/null
+rm -rf /data/adb/modules_update/Surfingtile 2>/dev/null
+rm -f /data/adb/modules_update/Surfingtile/update 2>/dev/null
